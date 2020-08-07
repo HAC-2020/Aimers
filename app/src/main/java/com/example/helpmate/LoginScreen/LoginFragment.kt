ad2c4109998e6bc2925f2aa10159dd994be311c7
tarak <ktarkeshwar305@gmail.com>
2020-08-07 18:36:59 +0530

    Eng Dates Added

--- app/src/main/java/com/example/helpmate/LoginScreen/LoginFragment.kt
+++ app/src/main/java/com/example/helpmate/LoginScreen/LoginFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.helpmate.R
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LoginFragment : Fragment() {
 
@@ -17,7 +19,9 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_login, container, false)
 
-
+        view.btn_eng.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_indexEng)
+        }
         return view
     }
 
