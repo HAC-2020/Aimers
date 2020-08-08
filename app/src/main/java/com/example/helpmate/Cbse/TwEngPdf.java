88873664bdac708f07911430dae796195acd8bdd
coder2699 <pranshu0414@gmail.com>
2020-08-08 15:15:43 +0530

    Doc 2 Added

---
+++ app/src/main/java/com/example/helpmate/Cbse/TwEngPdf.java
@@ -0,0 +1,80 @@
+package com.example.helpmate.Cbse;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.helpmate.R;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class TwEngPdf extends AppCompatActivity {
+    private TextView text1;
+    private PDFView pdfView;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference mhref;
+
+    public TwEngPdf() {
+        this.mhref = this.database.getReference("CbEngTw");
+    }
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_pdf_edpol);
+        this.pdfView = (PDFView)this.findViewById(R.id.pdf);
+        this.text1 = (TextView)this.findViewById(R.id.text1);
+        this.mhref.addValueEventListener(new ValueEventListener() {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = (String)dataSnapshot.getValue(String.class);
+                TwEngPdf.this.text1.setText(value);
+//                Toast.makeText(nineMathPdf.this, "Updated", Toast.LENGTH_SHORT).show();
+                String url = TwEngPdf.this.text1.getText().toString();
+                (TwEngPdf.this.new RetrivePdfStream()).execute(new String[]{url});
+            }
+
+            public void onCancelled( DatabaseError databaseError) {
+                Toast.makeText(TwEngPdf.this, "Failed to Update", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    class RetrivePdfStream extends AsyncTask<String, Void, InputStream> {
+        RetrivePdfStream() {
+        }
+
+        protected InputStream doInBackground(String... strings) {
+            BufferedInputStream inputStream = null;
+
+            try {
+                URL uRl = new URL(strings[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection)uRl.openConnection();
+                if (urlConnection.getResponseCode() == 200) {
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                }
+
+                return inputStream;
+            } catch (IOException var5) {
+                return null;
+            }
+        }
+
+        protected void onPostExecute(InputStream inputStream) {
+            TwEngPdf.this.pdfView.fromStream(inputStream).load();
+        }
+    }
+}
--- app/src/main/java/com/example/helpmate/Cbse/XiiSubFragment.kt
+++ app/src/main/java/com/example/helpmate/Cbse/XiiSubFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.example.helpmate.R
+import kotlinx.android.synthetic.main.fragment_xii_sub.view.*
 
 class XiiSubFragment : Fragment() {
 
@@ -16,8 +17,24 @@ class XiiSubFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_xii_sub, container, false)
-
-
+        view.mths.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_xiiSubFragment_to_twMathPdf)
+        }
+        view.chem.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_xiiSubFragment_to_twChemPdf)
+        }
+        view.phy.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_xiiSubFragment_to_twPhyPdf)
+        }
+        view.english.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_xiiSubFragment_to_twEngPdf)
+        }
+        view.bio.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_xiiSubFragment_to_twBioPdf)
+        }
+        view.phe.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_xiiSubFragment_to_twPhePdf)
+        }
         return view
     }
 
