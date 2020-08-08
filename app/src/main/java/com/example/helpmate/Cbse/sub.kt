8759f7f0b125034b490eb05e294f88a5a5085315
coder2699 <pranshu0414@gmail.com>
2020-08-08 15:55:37 +0530

    Checkpoint Commit

--- app/src/main/java/com/example/helpmate/Cbse/sub.kt
+++ app/src/main/java/com/example/helpmate/Cbse/sub.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.example.helpmate.R
+import kotlinx.android.synthetic.main.fragment_sub.view.*
 
 
 class sub : Fragment() {
@@ -17,6 +18,25 @@ class sub : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_sub, container, false)
+        view.maths.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_sub_to_nineMathPdf)
+        }
+        view.sci.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_sub_to_ninePdfSci)
+        }
+        view.ssc.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_sub_to_ninePdfSsc)
+        }
+        view.english.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_sub_to_ninePdfEng)
+        }
+        view.hindi.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_sub_to_ninePdfHindi)
+        }
+        view.comp.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_sub_to_ninePdfComp2)
+        }
+
         return view
     }
 }
